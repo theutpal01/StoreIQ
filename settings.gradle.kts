6c3bb95eeaccb12a0e3941c2136ddda04523635e
Utpal <dev.theutpal@gmail.com>
2026-09-07 22:05:11 +0530

    Fix CI: Use explicit Google Maven URL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
+        maven { url = uri("https://maven.google.com") }
         mavenCentral()
     }
 }
